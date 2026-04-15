8d5636373412a5e46667e36d89ec2e6017a01713
paulosspereira-code <paulosergiosouzapereira@gmail.com>
2026-04-15 14:00:27 -0300

    Implementando interfaces dos repositorios base

---
+++ deseulance/Application/Common/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Application.Common.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task AddAsync(T entity, CancellationToken cancellationToken);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ deseulance/Application/Common/Interfaces/Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Application.Common.Interfaces.Repositories
+{
+    public interface IReadOnlyRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken);
+    }
+}
