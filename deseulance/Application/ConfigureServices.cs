e95e66259ff26f83cb16757be0546506c5d1f64b
paulosspereira-code <paulosergiosouzapereira@gmail.com>
2026-04-15 17:20:39 -0300

    Implementação de exceptions, packages como mapster, mediatr, fluenvalidation

--- deseulance/Application/ConfigureServices.cs
+++ deseulance/Application/ConfigureServices.cs
@@ -1,6 +1,10 @@
-﻿using Application.Common.Interfaces.Services;
+﻿using Application.Common.Behaviours;
+using Application.Common.Interfaces.Services;
 using Application.Services;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Application
 {
@@ -8,6 +12,17 @@ namespace Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddAutoMapper(cfg => { }, typeof(ConfigureServices).Assembly);
+            
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            });
+
             services.AddScoped<IAuctionBidService, AuctionBidService>();
            
             services.AddScoped<IUserService, UserService>();
