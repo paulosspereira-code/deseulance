e5c6da6feead79884753237911df8f312e822586
produzesistemas <produzesistemas@gmail.com>
2026-04-15 10:45:19 -0300

    Criação dos mappings e dbcontext

---
+++ deseulance/Infrastructure/Mappings/UserClientMap.cs
@@ -0,0 +1,28 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Mappings;
+
+public class UserClientMap : IEntityTypeConfiguration<UserClient>
+{
+    public void Configure(EntityTypeBuilder<UserClient> builder)
+    {
+        builder.HasKey(x => x.IdUserClient);
+
+        builder.Property(x => x.IdUserClient)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Password2)
+            .IsRequired()
+            .HasMaxLength(255);
+    }
+}
